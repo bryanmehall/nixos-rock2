a2ec12aa901f948029e14359d4c9fba6ffc82083
Bryan Mehall <1575089+bryanmehall@users.noreply.github.com>
2024-10-11 19:34:19 -0600

    switch nixpkgs to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A minimal NixOS configuration for the Radxa Rock2 (RK3528A)";
 
   inputs = {
-    nixpkgs.url = "git+file:../nixpkgs"; #"github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
     nixos-generators = {
